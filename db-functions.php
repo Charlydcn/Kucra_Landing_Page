5106f1224fb2b020f9a2e7e8fca4aba389aa5ea0
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-17 11:05:18 +0200

    (db-functions) db connexion in dedicated function

--- db-functions.php
+++ db-functions.php
@@ -1,11 +1,22 @@
 <?php
 
-try {
-    $db = new PDO('mysql:host=localhost;dbname=forfaits;charset=utf8', 'root', '');
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
+function dbConnect()
+{
+    try {
+
+        $db = new PDO('mysql:host=localhost;dbname=forfaits;charset=utf8', 'root', '');
+
+    } catch (PDOException $e) {
+
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+
+    }
+
+    return $db;
 }
 
+$db = dbConnect();
 $sql =
     "SELECT *
      FROM pricing
@@ -17,11 +28,24 @@ $pricings = $pricingStatement->fetchAll();
 
 function UpdatePricing($name, $price, $sale, $bandwidth, $onlineSpace, $support, $domain, $hiddenFees, $id)
 {
+    $db = dbConnect();
     $sql = 
     "UPDATE pricing
     SET name = :name, price = :price, sale = :sale, bandwidth = :bandwidth, online_space = :onlineSpace,
     support = :support, domain = :domain, hidden_fees = :hiddenFees
-    WHERE id = :id";    
+    WHERE id_pricing = :id";
+    
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':name', $name);
+    $stmt->bindValue(':price', $price);
+    $stmt->bindValue(':sale', $sale);
+    $stmt->bindValue(':bandwidth', $bandwidth);
+    $stmt->bindValue(':onlineSpace', $onlineSpace);
+    $stmt->bindValue(':support', $support);
+    $stmt->bindValue(':domain', $domain);
+    $stmt->bindValue(':hiddenFees', $hiddenFees);
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
 }
 
 ?>
