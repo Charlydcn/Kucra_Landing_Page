14a77afa7ebc2d7760271ff34e3fdd8a4d65b22f
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-17 11:07:21 +0200

    (functions) fixed booleens to be  true or false

--- functions.php
+++ functions.php
@@ -6,12 +6,11 @@ require 'db-functions.php';
 
 if (isset($_GET['action'])) {
 
-    $id = (isset($_GET['id'])) ? $_GET['id'] : null;    
+    $id = (isset($_GET['id'])) ? $_GET['id'] : null;   
 
     switch($_GET['action']) {
 
-        case "updatePricing":
-
+        case "update":
             $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT);
             $sale = filter_input(INPUT_POST, "sale", FILTER_VALIDATE_FLOAT);
@@ -19,12 +18,22 @@ if (isset($_GET['action'])) {
             $onlineSpace = filter_input(INPUT_POST, "online_space", FILTER_VALIDATE_FLOAT);
             $support = filter_input(INPUT_POST, "support", FILTER_VALIDATE_BOOLEAN);
             $domain = filter_input(INPUT_POST, "domain", FILTER_VALIDATE_FLOAT);
-            $hiddenFees = filter_input(INPUT_POST, "hiddenFees", FILTER_VALIDATE_BOOLEAN);
+            $hiddenFees = filter_input(INPUT_POST, "hidden_fees", FILTER_VALIDATE_BOOLEAN);
+            
+            if ($support === NULL) {
+                $support = false;
+            }
+
+            if ($hiddenFees === NULL) {
+                $hiddenFees = false;
+            }            
 
             if ($price > 0 && $bandwidth > 0 && $onlineSpace > 0) {
                 
-                if($name && $price && $sale && $bandwidth && $onlineSpace && $support && $domain && $hiddenFees) {
-                    UpdatePricing($name, $price, $sale, $bandwidth, $onlineSpace, $support, $domain, $hiddenFees, $id);             
+                if($name && $price && $bandwidth && $onlineSpace && $domain) {
+
+                    UpdatePricing($name, $price, $sale, $bandwidth, $onlineSpace, $support, $domain, $hiddenFees, $id);                    
+                    
                 }               
 
             }
