7db9596df5d7aa51b23d843194f937c3b6d9e9a9
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-18 09:34:07 +0200

    (admin) removed useless label for submit input

--- paneladmin.php
+++ paneladmin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 require_once 'db-functions.php';
@@ -6,6 +6,7 @@ require_once 'db-functions.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,73 +14,73 @@ require_once 'db-functions.php';
     <title>Admin dashboard</title>
     <link rel="stylesheet" href="css/paneladmin.css">
 </head>
+
 <body>
 
     <h2>Update Pricing</h2>
-    
+
     <div id="price_update_cards">
 
-        <?php 
-        
-        foreach ($pricings as $pricing) { 
+        <?php
+
+        foreach ($pricings as $pricing) {
 
         ?>
 
-        <div class="price_update_card">
-            <form action="functions.php?action=update&id='<?=$pricing['id_pricing']?>'" method="POST" enctype="multipart/form-data" autocomplete="off">
-                <label>
-                    <p>Name</p>
-                    <input type="text" name="name" value= <?= $pricing['name'] ?> required >
-                </label>
+            <div class="price_update_card">
+                <form action="functions.php?action=update&id='<?= $pricing['id_pricing'] ?>'" method="POST" autocomplete="off">
+                    <label>
+                        <p>Name</p>
+                        <input type="text" name="name" value=<?= $pricing['name'] ?> required>
+                    </label>
 
-                <label>
-                    <p>Price</p>
-                    <input type="number" name="price" value= <?= $pricing['price'] ?> required >
-                </label>
+                    <label>
+                        <p>Price</p>
+                        <input type="number" name="price" value=<?= $pricing['price'] ?> required>
+                    </label>
 
-                <label>
-                    <p>Sale</p>
-                    <input type="number" name="sale" value= <?= $pricing['sale'] ?> required >
-                </label>
+                    <label>
+                        <p>Sale</p>
+                        <input type="number" name="sale" value=<?= $pricing['sale'] ?> required>
+                    </label>
 
-                <label>
-                    <p>Bandwidth</p>
-                    <input type="number" name="bandwidth" value= <?= $pricing['bandwidth'] ?> required >
-                </label>
+                    <label>
+                        <p>Bandwidth</p>
+                        <input type="number" name="bandwidth" value=<?= $pricing['bandwidth'] ?> required>
+                    </label>
 
-                <label>
-                    <p>Online Space</p>
-                    <input type="number" name="onlineSpace" value= <?= $pricing['onlineSpace'] ?> required >
-                </label>
+                    <label>
+                        <p>Online Space</p>
+                        <input type="number" name="onlineSpace" value=<?= $pricing['onlineSpace'] ?> required>
+                    </label>
 
-                <label>
-                    <p>Support</p>
-                    <input type="checkbox" name="support" <?php if ($pricing['support'] == 1) {echo "checked";} ?> >
-                </label>
+                    <label>
+                        <p>Support</p>
+                        <input type="checkbox" name="support" <?php if ($pricing['support'] == 1) { echo "checked"; } ?>>
+                    </label>
 
-                <label>
-                    <p>Domain</p>
-                    <input type="text" name="domain" value= <?= $pricing['domain'] ?> required >
-                </label>
+                    <label>
+                        <p>Domain</p>
+                        <input type="text" name="domain" value=<?= $pricing['domain'] ?> required>
+                    </label>
 
-                <label>
-                    <p>Hidden Fees</p>
-                    <input type="checkbox" name="hiddenFees" <?php if ($pricing['hiddenFees'] == 1) {echo "checked";} ?> >
-                </label>
+                    <label>
+                        <p>Hidden Fees</p>
+                        <input type="checkbox" name="hiddenFees" <?php if ($pricing['hiddenFees'] == 1) { echo "checked"; } ?>>
+                    </label>
 
-                <label name="submit">
                     <input type="submit" value="Update">
-                </label>
-            </form>
+                </form>
 
-        </div>
+            </div>
 
-        <?php } 
-        
+        <?php }
+
 
         ?>
 
     </div>
 
 </body>
+
 </html>
