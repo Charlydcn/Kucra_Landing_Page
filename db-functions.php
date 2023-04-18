0bd2372ee23b8e406dbbc44f3b5091f20daf05fe
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-18 11:57:12 +0200

    (DB FONC) fixed bools & put dbConnect in function

--- db-functions.php
+++ db-functions.php
@@ -5,47 +5,48 @@ function dbConnect()
     try {
 
         $db = new PDO('mysql:host=localhost;dbname=forfaits;charset=utf8', 'root', '');
-
     } catch (PDOException $e) {
 
         print "Erreur !: " . $e->getMessage() . "<br/>";
         die();
-
     }
 
     return $db;
 }
 
-$db = dbConnect();
-$sql =
-    "SELECT *
-     FROM pricing
-     LIMIT 3";
-
-$pricingStatement = $db->prepare($sql);
-$pricingStatement->execute();
-$pricings = $pricingStatement->fetchAll();
-
-function UpdatePricing($name, $price, $sale, $bandwidth, $onlineSpace, $support, $domain, $hiddenFees, $id)
-{
+function getPricings() {
     $db = dbConnect();
-    $sql = 
-    "UPDATE pricing
-    SET name = :name, price = :price, sale = :sale, bandwidth = :bandwidth, onlineSpace = :onlineSpace,
-    support = :support, domain = :domain, hiddenFees = :hiddenFees
-    WHERE id_pricing = :id";
-    
+    $sql =
+        "SELECT *
+        FROM pricing
+        LIMIT 3";
+
+    $pricingStatement = $db->prepare($sql);
+    $pricingStatement->execute();
+    $pricings = $pricingStatement->fetchAll();
+
+    return $pricings;
+}
+
+function updatePricing($name, $price, $sale, $bandwidth, $onlineSpace, $support, $domain, $hiddenFees, $id)
+{
+
+    $db = dbConnect();
+    $sql =
+        "UPDATE pricing
+        SET name = :name, price = :price, sale = :sale, bandwidth = :bandwidth, onlineSpace = :onlineSpace,
+        support = :support, domain = :domain, hiddenFees = :hiddenFees
+        WHERE id_pricing = :id";
+
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':name', $name);
     $stmt->bindValue(':price', $price);
-    $stmt->bindValue(':sale', $sale);
+    $stmt->bindValue(':sale', $sale === null || $sale === false ? null : $sale);
     $stmt->bindValue(':bandwidth', $bandwidth);
     $stmt->bindValue(':onlineSpace', $onlineSpace);
-    $stmt->bindValue(':support', $support);
+    $stmt->bindValue(':support', $support ? 1 : 0);
     $stmt->bindValue(':domain', $domain);
-    $stmt->bindValue(':hiddenFees', $hiddenFees);
+    $stmt->bindValue(':hiddenFees', $hiddenFees ? 1 : 0);
     $stmt->bindValue(':id', $id);
     $stmt->execute();
 }
-
-?>
