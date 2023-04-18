7e62d6eda8bc04981924424f65b7fa5f936fa094
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-18 11:55:36 +0200

    (functions) fixed case update (operationnal now)

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-require 'db-functions.php';
+require_once 'db-functions.php';
 
 
 if (isset($_GET['action'])) {
@@ -11,33 +11,36 @@ if (isset($_GET['action'])) {
     switch ($_GET['action']) {
 
         case "update":
-            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT);
-            $sale = filter_input(INPUT_POST, "sale", FILTER_VALIDATE_FLOAT);
-            $bandwidth = filter_input(INPUT_POST, "bandwidth", FILTER_VALIDATE_FLOAT);
-            $onlineSpace = filter_input(INPUT_POST, "onlineSpace", FILTER_VALIDATE_FLOAT);
-            $support = filter_input(INPUT_POST, "support", FILTER_VALIDATE_BOOLEAN);
-            $domain = filter_input(INPUT_POST, "domain", FILTER_VALIDATE_FLOAT);
-            $hiddenFees = filter_input(INPUT_POST, "hiddenFees", FILTER_VALIDATE_BOOLEAN);
-
-            if ($support === NULL) {
-                $support = false;
-            }
-
-            if ($hiddenFees === NULL) {
-                $hiddenFees = false;
-            }
-
-            if ($price > 0 && $bandwidth > 0 && $onlineSpace > 0) {
-
-                if ($name && $price && $bandwidth && $onlineSpace && is_bool($support) && $domain && is_bool($hiddenFees)) {
-
-                    UpdatePricing($name, $price, $sale, $bandwidth, $onlineSpace, $support, $domain, $hiddenFees, $id);
-                    var_dump($name);
+            if (isset($_POST['submit'])) {
+                $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_INT);
+                $sale = filter_input(INPUT_POST, "sale", FILTER_VALIDATE_INT);
+                $bandwidth = filter_input(INPUT_POST, "bandwidth", FILTER_VALIDATE_INT);
+                $onlineSpace = filter_input(INPUT_POST, "onlineSpace", FILTER_VALIDATE_INT);
+                $support = filter_input(INPUT_POST, "support", FILTER_VALIDATE_BOOLEAN);
+                $domain = filter_input(INPUT_POST, "domain", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $hiddenFees = filter_input(INPUT_POST, "hiddenFees", FILTER_VALIDATE_BOOLEAN);
+    
+                // Si il y avait beaucoup de booléens, il aurait fallu faire un tableau de valeurs, et transformer chaque valeurs en bool(false) si null
+                if ($support === null) {
+                    $support = false;
                 }
+    
+                if ($hiddenFees === null) {
+                    $hiddenFees = false;
+                }
+    
+                if ($price > 0 && $bandwidth > 0 && $onlineSpace > 0) {
+    
+                    if ($name && $price && $bandwidth && $onlineSpace && is_bool($support) && $domain && is_bool($hiddenFees)) {
+    
+                        updatePricing($name, $price, $sale, $bandwidth, $onlineSpace, $support, $domain, $hiddenFees, $id);
+                    }
+                }
+    
+                header("Location:paneladmin.php");
             }
 
-            // header("Location:paneladmin.php");
-            break;
+        break;
     }
 }
