27cb7eac0bd062929963397962049b81d59d6944
Charlydcn <charly.ducournau@outlook.fr>
2023-04-19 10:26:07 +0200

    (BDD) updated database with new table "email"

--- BDD/forfaits.sql
+++ BDD/forfaits.sql
@@ -1,60 +1,52 @@
 -- --------------------------------------------------------
 -- Hôte:                         127.0.0.1
--- Version du serveur:           5.7.33 - MySQL Community Server (GPL)
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
 -- SE du serveur:                Win64
--- HeidiSQL Version:             11.2.0.6213
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Listage de la structure de la base pour forfaits
-CREATE DATABASE IF NOT EXISTS `forfaits` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `forfaits`;
+-- Listage de la structure de la base pour kucra
+CREATE DATABASE IF NOT EXISTS `kucra` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `kucra`;
 
--- Listage de la structure de la table forfaits. pricing
+-- Listage de la structure de table kucra. email
+CREATE TABLE IF NOT EXISTS `email` (
+  `id_email` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table kucra. pricing
 CREATE TABLE IF NOT EXISTS `pricing` (
-  `id_pricing` int(11) NOT NULL,
+  `id_pricing` int NOT NULL,
   `name` varchar(25) NOT NULL,
-  `price` int(11) NOT NULL,
-  `sale` int(11) DEFAULT NULL,
-  `bandwidth` int(11) NOT NULL DEFAULT '0',
-  `onlineSpace` int(11) NOT NULL,
+  `price` int NOT NULL,
+  `sale` int DEFAULT NULL,
+  `bandwidth` int NOT NULL DEFAULT '0',
+  `onlineSpace` int NOT NULL,
   `support` tinyint(1) NOT NULL DEFAULT '0',
   `domain` varchar(50) NOT NULL DEFAULT '0',
   `hiddenFees` tinyint(1) NOT NULL DEFAULT '0',
-  `nbMember` int(11) DEFAULT NULL,
+  `nbMember` int DEFAULT NULL,
   PRIMARY KEY (`id_pricing`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- -- Listage des données de la table forfaits.pricing : ~3 rows (environ)
--- /*!40000 ALTER TABLE `pricing` DISABLE KEYS */;
--- INSERT INTO `pricing` (`id_pricing`, `name`, `price`, `sale`, `bandwidth`, `onlineSpace`, `support`, `domain`, `hiddenFees`, `nbMember`) VALUES
--- 	(1, 'Started', 9, 0, 1000, 500, 0, '1', 0, 0),
--- 	(2, 'Advanced', 19, 20, 2000, 1000, 1, '3', 0, 2),
--- 	(3, 'Professional', 29, NULL, 3000, 2000, 1, 'Unlimited', 0, 1);
--- /*!40000 ALTER TABLE `pricing` ENABLE KEYS */;
+-- Les données exportées n'étaient pas sélectionnées.
 
--- /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
--- /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
--- /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
-
--- Listage des données de la table forfaits.pricing : ~0 rows (environ)
-/*!40000 ALTER TABLE `pricing` DISABLE KEYS */;
-INSERT INTO `pricing` (`id_pricing`, `name`, `price`, `sale`, `bandwidth`, `onlineSpace`, `support`, `domain`, `hiddenFees`) VALUES
-	(1, 'Starter', 9, NULL, 1, 500, 0, '1', 0),
-	(2, 'Advanced', 19, 20, 2, 1, 1, '3', 0),
-	(3, 'Professional', 29, NULL, 3, 2, 1, 'Unlimited', 0);
-/*!40000 ALTER TABLE `pricing` ENABLE KEYS */;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
-
